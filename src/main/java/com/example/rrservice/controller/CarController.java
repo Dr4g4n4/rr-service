ac4cb01ddbcfd4c6a2a3293b04bc2d235ca2f34e
Dr4g4n4 <draganamihajlovic55@yahoo.com>
2021-10-28 10:49:22 +0200

    Created kafka producer and made kafka configuration class.Made api for adding car.

--- src/main/java/com/example/rrservice/controller/CarController.java
+++ src/main/java/com/example/rrservice/controller/CarController.java
@@ -1,20 +1,38 @@
 package com.example.rrservice.controller;
 
+import com.example.rrservice.model.Car;
+import com.example.rrservice.model.EngineType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("/car")
 public class CarController {
 
+    @Autowired
+    private KafkaTemplate<String, Car> kafkaTemplate;
+    private static final String topic = "second";
+
+    EngineType engineType = new EngineType(10,10,10,10,"DIZEL");
+    Car car = new Car("45dg4", engineType,"44rdg", "passat","208",new Date(), new Date(),15.0);
+
     @GetMapping(value = "/test/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> testMethod (@PathVariable String name){
         System.out.println("name " + name);
+        kafkaTemplate.send(topic,car);
         return new ResponseEntity<String>("OK", HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createCar(@RequestBody Car car){
+        kafkaTemplate.send(topic,car);
+        return new ResponseEntity<String>("ACCEPTED", HttpStatus.OK);
+
     }
 }
